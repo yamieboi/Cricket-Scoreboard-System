54948663892c1e3e0d4451384da9f5d968778ec5
Yamie <90822795+yamieboi@users.noreply.github.com>
2025-10-08 22:24:01 +0600

    initial push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1 style="font-family: Outfit">
+    Scoreboard App <br>
+    Funtionalities over beauty <br>
+    Developed by Hasan M. Mahim
+</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
