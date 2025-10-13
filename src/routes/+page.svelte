2f53a54e0449a674e7e98d1f51bbd2f382db83ad
Yamie <90822795+yamieboi@users.noreply.github.com>
2025-10-13 22:28:20 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,38 @@
-<h1 style="font-family: Outfit">
-    Scoreboard App <br>
-    Funtionalities over beauty <br>
-    Developed by Hasan M. Mahim
-</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="big-brother">
+    <div style="font-size:2vh">
+        <h1 style="font-family: Outfit; user-select:none; text-align:center; color:rgba(0, 0, 0, 0.6); ">
+            <span style="color:rgba(0, 0, 0, 0.8);">Cricket Scoreboard</span> <br>
+            Funtionality over beauty <br>
+            Developed by <span style="color:rgba(0, 0, 0, 0.8)">Hasan M. Mahim</span>
+        </h1>
+    </div>
+
+    <div class="match-setup">
+        <div class="match-setup-title" style="font-family: Outfit; font-size: 3vh; padding-top: 1vh; font-weight: 500; user-select:none; text-align:center; color:rgba(255, 255, 255, 0.8);">Match Setup</div>
+    </div>
+</div>
+
+
+<style>
+    .big-brother{
+        display: flex;
+        align-content: center;
+        column-gap: 1px;
+        align-items: center;
+        justify-items: center;
+        gap: 1px 1px;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: center;
+    }
+    .match-setup{
+        height:80vh;
+        width:50vw;
+        color: rgb(255, 255, 255);
+        background-color: rgba(3, 3, 3, 0.5);
+        position: relative;
+        border-radius: 5px;
+    }
+/*     :global(body){
+    } */
+</style>
